d4f028cdb9e0cdc50b794496ed4403093c0675ba
ezio84 <brabus84@gmail.com>
2017-09-03 16:45:40 +0200

    Allow doubletap/longpress power to toggle torch -11- [1/2]
    NB: for further flashlight action implementations (e.g. squeeze for
    flashlight) when screen is ON, just call toggleCameraFlash(),
    the following statusbar code is already dynamic and able to
    check proximity if screen is off, and to toggle flashlight straight
    if screen is ON
    ---
    
    Thanks beanstown106 and lineage guys for the initial longpress action
    bringup in PhoneWindowManager back in the days
    
    If the user enables double tap for camera action, torch action will
    automatically switch to long press mode.
    If the user enables double tap for torch, double tap for camera
    will be disabled accordingly.
    
    Remove the settings pref is the device doesn't have flashlight
    
    A 170ms delay for double tap action is not annoying on single tap
    to wake up the device.
    
    Vibrate when toggling torch to let the user know of unwanted toggling
    in the pocket (even if vibration is disabled by user)
    
    Allow torch action also on ambient display
    
    Change-Id: I12da044f86c7b625872607529cf8524615cf576b
    
    Power button flashlight toggle: add proximity sensor check
    
    cm/lineage guys managed the proximity sensor in a really nice and reliable
    way for the proximity wake feature, so let's kang and adapt it to block
    flashlight toggling when the sensor is covered.
    
    Change-Id: I0851df54341c4223e90a8926ca762c408c4a2b89
    
    Power button flashlight: allow to skip proximity check per device
    
    set config_flashlightProximityTimeout to -1
    
    some devices have an ultrasound proximity sensor that seems unable
    to work when screen is off
    
    Change-Id: I82cc1cbf123823987e7ae45e52c29a37d4d3936d
    
    Power button flashlight toggle: rely on aosp doze sensors check code
    
    Till now we were checking proximity sensor before toggling the
    flashlight with our custom implementation.
    For doze pulsing, Google have a solid code implementation to check the
    same, and since some months they even made it better using a
    prox gated brightness sensor if available (e.g. Pixel phones).
    Let's just use it.
    
    Power button flashlight toggle: rely on aosp doze sensors check code
    
    Till now we were checking proximity sensor before toggling the
    flashlight with our custom implementation.
    For doze pulsing, Google have a solid code implementation to check the
    same, and since some months they even made it better using a
    prox gated brightness sensor if available (e.g. Pixel phones).
    Let's just use it.
    
    Flashlight toggle API: extend to screenOn state
    
    Change-Id: Id25d1bbb81caee0c9c362c917df269d78d064340
    
    Power flashlight: allow to use it when all ambient/wake options are off
    
    now that we linked the proximity sensor check with aosp ambient code,
    we need to enable doze services also when the user disables all ambient
    and wake options

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1318,7 +1318,6 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         // Private API call to make the shadows look better for Recents
         ThreadedRenderer.overrideProperty("ambientRatio", String.valueOf(1.5f));
-
     }
 
     @NonNull
@@ -2162,6 +2161,14 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     @Override
     public void toggleCameraFlash() {
+        if (!isScreenFullyOff() && mDeviceInteractive && !isPulsing() && !mDozing) {
+            toggleFlashlight();
+            return;
+        }
+        mDozeServiceHost.toggleFlashlightProximityCheck();
+    }
+
+    public void toggleFlashlight() {
         if (mFlashlightController != null) {
             mFlashlightController.initFlashLight();
             if (mFlashlightController.hasFlashlight() && mFlashlightController.isAvailable()) {
@@ -2743,10 +2750,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         pw.println("SharedPreferences:");
         for (Map.Entry<String, ?> entry : Prefs.getAll(mContext).entrySet()) {
             pw.print("  "); pw.print(entry.getKey()); pw.print("="); pw.println(entry.getValue());
-        }
-
-        if (mFlashlightController != null) {
-            mFlashlightController.dump(fd, pw, args);
         }
     }
 
