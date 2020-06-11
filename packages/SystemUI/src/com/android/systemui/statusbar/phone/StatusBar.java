2ee9603c5c7a725bcf60bdc563a011427ad98f6d
HrX03 <dn.bianco03@gmail.com>
2020-06-11 20:40:49 +0200

    fwb: Implement VolumePluginManager
    Change-Id: I29bfc091df7a13ece3c859310e10c75daffc064d
    Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -607,6 +607,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private PulseController mPulseController;
     private VisualizerView mVisualizerView;
+    private VolumePluginManager mVolumePluginManager;
 
     private final BroadcastReceiver mWallpaperChangedReceiver = new BroadcastReceiver() {
         @Override
@@ -983,6 +984,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         mKeyguardManager = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
         mWallpaperSupported =
                 mContext.getSystemService(WallpaperManager.class).isWallpaperSupported();
+
+        mVolumePluginManager = new VolumePluginManager(mContext, mHandler);
 
         // Connect in to the status bar manager service
         mCommandQueue.addCallback(this);
