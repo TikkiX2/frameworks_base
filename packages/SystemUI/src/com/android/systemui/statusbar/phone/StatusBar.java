ceb1110ae927784e1e79322a830d3ab726c3f1ba
NurKeinNeid <mralexman3000@gmail.com>
2021-01-11 21:08:11 +0100

    Revert "base: Add navigation bar visibility control [1/2]"
    This reverts commit d60b759eb12bf97ea1f22e2195188a81d25903b9.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -707,7 +707,6 @@ public class StatusBar extends SystemUI implements DemoMode,
     private final BubbleController mBubbleController;
     private final BubbleController.BubbleExpandListener mBubbleExpandListener;
     private final IFingerprintService mFingerprintService;
-    private boolean mShowNavBar;
 
     private ActivityIntentHelper mActivityIntentHelper;
 
@@ -1185,7 +1184,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mNotificationPanelViewController.setHeadsUpManager(mHeadsUpManager);
         mNotificationLogger.setHeadsUpManager(mHeadsUpManager);
 
-        updateNavigationBar(true);
+        createNavigationBar(result);
 
         if (ENABLE_LOCKSCREEN_WALLPAPER && mWallpaperSupported) {
             mLockscreenWallpaper = mLockscreenWallpaperLazy.get();
@@ -2076,9 +2075,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.QS_SHOW_BATTERY_PERCENT),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.FORCE_SHOW_NAVBAR),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_TILE_STYLE),
                     false, this, UserHandle.USER_ALL);
         }
@@ -2096,9 +2092,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 setPulseOnNewTracks();
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_SHOW_BATTERY_PERCENT))) {
                 setQsBatteryPercentMode();
-            } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.FORCE_SHOW_NAVBAR))) {
-                updateNavigationBar(false);
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_TILE_STYLE))) {
                 stockTileStyle();
                 updateTileStyle();
@@ -2111,7 +2104,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             setLockScreenMediaBlurLevel();
             setPulseOnNewTracks();
             setQsBatteryPercentMode();
-            updateNavigationBar(false);
         }
     }
 
@@ -2146,24 +2138,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (getNavigationBarView() != null) {
             getNavigationBarView().setBlockedGesturalNavigation(blocked);
         }
-    }
-
-    private void updateNavigationBar(boolean init) {
-        boolean showNavBar = ActionUtils.deviceSupportNavigationBar(mContext);
-        if (init) {
-            if (showNavBar) {
-                mNavigationBarController.createNavigationBars(true, null);
-            }
-        } else {
-            if (showNavBar != mShowNavBar) {
-                if (showNavBar) {
-                    mNavigationBarController.createNavigationBars(true, null);
-                } else {
-                    mNavigationBarController.removeNavigationBar(mDisplayId);
-                }
-            }
-        }
-        mShowNavBar = showNavBar;
     }
 
     /**
