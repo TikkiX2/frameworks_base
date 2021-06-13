crdroidandroid/android_frameworks_base

FOD icon for oneplus 8 series
	*needed for Oneplus 8/8pro/8t and Nord
	*can be set by using this overlay:
	<integer name="config_fod_pressed_color">4</integer>

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -147,7 +147,8 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         R.drawable.fod_icon_pressed,
         R.drawable.fod_icon_pressed_cyan,
         R.drawable.fod_icon_pressed_green,
-        R.drawable.fod_icon_pressed_yellow
+        R.drawable.fod_icon_pressed_yellow,
+        R.drawable.fod_icon_pressed_ffffe6
     };
 
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =

TikkiX2/frameworks_base

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -135,7 +135,8 @@ public class FODCircleView extends ImageView {
         R.drawable.fod_icon_pressed,
         R.drawable.fod_icon_pressed_cyan,
         R.drawable.fod_icon_pressed_green,
-        R.drawable.fod_icon_pressed_yellow
+        R.drawable.fod_icon_pressed_yellow,
+        R.drawable.fod_icon_pressed_light_yellow
     };
 
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =